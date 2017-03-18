bae195ffceb5e1041edb106027af0821cdc8cf0e
JafarAkhondali <jafar.akhondali@yahoo.com>
2017-03-18 15:54:11 +0330

    Newer comment

--- src/algorithm/pkg95/pkg2/Algorithm952.java
+++ src/algorithm/pkg95/pkg2/Algorithm952.java
@@ -1,6 +1,7 @@
 // added this comment just for test
 
 //New comment 
+//Newer comment as guest
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
